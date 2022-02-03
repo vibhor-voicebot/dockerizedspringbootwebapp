27a50093ab83fb89de6146f8a6338ba89b8685be
Vibhor Saxena <vibhorsaxena2@gmail.com>
2022-02-03 07:29:48 +0000

    AB#37

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -14,7 +14,7 @@ public class Example {
     @RequestMapping(path = "/index", produces = "application/json; charset=UTF-8" )
     public String home() throws JSONException  {
     	JSONObject entity = new JSONObject();
-    	entity.put("ContextRoot", "/landing");
+    	entity.put("ContextRoot", "/landinghomepage");
     	//HashMap<String, String> objMap = new HashMap<>();
     	Gson gson = new Gson();
         String json = gson.toJson(entity);
